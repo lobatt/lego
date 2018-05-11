8e03012e687fa3b1664249428ac05a2e45f85c7c
lobatt <lob4tt@gmail.com>
2018-05-11 20:33:05 +0800

    add event logging

--- logging/eventlog.go
+++ logging/eventlog.go
@@ -3,9 +3,13 @@ package logging
 import (
 	"encoding/json"
 	uuid "github.com/satori/go.uuid"
+	"io"
 	"log"
+	"os"
 	"time"
 )
+
+var eventLog = log.New(os.Stdout, "", 0)
 
 type EventLogEntry struct {
 	id        string
@@ -18,15 +22,19 @@ func newEventLogEntry(e interface{}) *EventLogEntry {
 	return entry
 }
 
+// SetOutput set the output io.Writer for global event logger
+func SetOutput(w io.Writer) {
+	eventLog.SetOutput(w)
+}
+
 // EventLog logs an event defined by caller in json format
-func EventLog(event interface{}) {
+func LogEvent(event interface{}) {
 	entry := newEventLogEntry(event)
 	logJson, e := json.Marshal(entry)
 	if e != nil {
-		log.Printf("{error: %s}\n", e.Error())
+		eventLog.Printf("{error: %s}\n", e.Error())
 		return
 	}
 
-	log.Printf("%s\n", logJson)
-
+	eventLog.Printf("%s\n", logJson)
 }
